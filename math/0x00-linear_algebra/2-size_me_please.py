fa0e96474b27fe6d58c10119824b240418deddef
JamesTeeters <4925@holbertonstudents.com>
2023-01-09 15:20:24 -0600

    added doc

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """get the shape of a matrix"""
+
+
 def matrix_shape(matrix):
+    """use recursion to get the length of all arrays"""
     try:
         return([len(matrix)] + matrix_shape(matrix[0]))
     except Exception:
