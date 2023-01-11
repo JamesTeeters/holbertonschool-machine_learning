2167a75a41d16f1cdaf1f5cf7514793143738406
JamesTeeters <4925@holbertonstudents.com>
2023-01-11 10:42:42 -0600

    numpy matrix multiplication

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""matrix multiplication function"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """multiply matrix using numpy multiplication"""
+    return np.matmul(mat1, mat2)
