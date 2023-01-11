ca7ef3838917cc0e1ca0b416695368b8732a9dd6
JamesTeeters <4925@holbertonstudents.com>
2023-01-11 10:13:27 -0600

    transpose using numpy

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""transpose a matrix using numpy's transpose"""
+
+
+def np_transpose(matrix):
+    """transpose a matrix using numpy's transpose"""
+    return matrix.transpose()
