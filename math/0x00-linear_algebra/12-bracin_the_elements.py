7459dc439f1beaf3dc4e667d5ca07b9295ad9d44
JamesTeeters <4925@holbertonstudents.com>
2023-01-11 10:31:40 -0600

    numpy amtrix calculator

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """performs element-wise addition, subtraction, multiplication, and division"""
-import numpy as np
 
 
 def np_elementwise(mat1, mat2):
     """
     performs element-wise addition, subtraction, multiplication, and division
     """
-    return np.add(mat1, mat2), np.subtract(mat1, mat2),\
-        np.multiply(mat1, mat2), np.divide(mat1, mat2)
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
