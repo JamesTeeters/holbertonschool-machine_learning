9c324ec4de26192c1a5c168fc7a2c883a5c33ea9
JamesTeeters <4925@holbertonstudents.com>
2023-01-24 17:27:04 -0600

    check if data is list'

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -13,7 +13,7 @@ class Poisson:
 
         if data is not None:
             """check to see if data exists"""
-            if type(data) != list:
+            if type(data) is not list:
                 """data is not a list"""
                 raise TypeError("data must be a list")
             elif len(data) < 2:
