ca4b757744ff506bcd7b44cff7ee7219d4130d4e
JamesTeeters <4925@holbertonstudents.com>
2023-01-24 17:19:17 -0600

    created poisson class

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+
+class Poisson:
+    """Class for Poisson"""
+    def __init__(self, data=None, lambtha=1.):
+        """docstring for Poisson"""
+        self.data = data
+        """Data = list of data points"""
+        self.lambtha = float(sum(data) / len(data)) if data else lambtha
+        """lambtha = mean number of data points"""
+
+        if data is not None:
+            """check to see if data exists"""
+            if type(data) != list:
+                """data is not a list"""
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                """data is less than 2"""
+                raise ValueError("data must have at least 2 elements")
+        else:
+            """data is None"""
+            if lambtha <= 0:
+                """lambtha must be greater than 0"""
+                raise ValueError("lambtha must be positive")
+            else:
+                """use lambtha as mean number of data points"""
+                data = lambtha
