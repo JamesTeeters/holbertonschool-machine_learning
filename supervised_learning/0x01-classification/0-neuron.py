3e04e7ee1ad4ef04404b637c84f98817bf8f2f2b
JamesTeeters <4925@holbertonstudents.com>
2023-02-04 16:24:52 -0600

    vixed TypeErro for nx

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -9,16 +9,17 @@ class Neuron:
         """initialize a neuron"""
         self.nx = nx
         """nx = the number of input features to the neuron"""
+
+        if type(self.nx) is not int:
+            """check if a neuron is an integer"""
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            """check if a neuron is positive"""
+            raise ValueError('nx must be a positive integer')
+        
         self.W = np.random.randn(1, nx)
         """w = weicght vector for neuron"""
         self.b = 0
         """b = bias"""
         self.A = 0
         """A = activated output of neuron"""
-
-        if type(nx) is not int:
-            """check if a neuron is an integer"""
-            raise TypeError('nx must be an integer')
-        if nx < 1:
-            """check if a neuron is positive"""
-            raise ValueError('nx must be a positive integer')
