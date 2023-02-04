c8b08d4851d215998ad987392a7a00126bd1eadc
JamesTeeters <4925@holbertonstudents.com>
2023-02-04 16:45:14 -0600

    added private instance varibles

--- supervised_learning/0x01-classification/1-neuron.py
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -1,1 +1,34 @@
 #!/usr/bin/env python3
+"""create a class for a neuron"""
+import numpy as np
+
+
+class Neuron:
+    """class for a neuron"""
+    def __init__(self, nx):
+        """initialize a neuron"""
+        self.nx = nx
+        """nx = the number of input features to the neuron"""
+
+        if type(self.nx) is not int:
+            """check if a neuron is an integer"""
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            """check if a neuron is positive"""
+            raise ValueError('nx must be a positive integer')
+
+        """public instance variables"""
+        self.W = np.random.randn(1, nx)
+        """w = weicght vector for neuron"""
+        self.b = 0
+        """b = bias"""
+        self.A = 0
+        """A = activated output of neuron"""
+
+        """private instance variables"""
+        self._W = np.random.randn(1, nx)
+        """w = weicght vector for neuron"""
+        self._b = 0
+        """b = bias"""
+        self._A = 0
+        """A = activated output of neuron"""
