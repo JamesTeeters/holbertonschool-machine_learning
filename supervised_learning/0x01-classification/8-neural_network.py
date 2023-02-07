6bc3e26e0117e1dd33e946a56b1678ba42326190
JamesTeeters <4925@holbertonstudents.com>
2023-02-07 12:20:23 -0600

    fixed b1

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -9,15 +9,15 @@ class NeuralNetwork:
         """initialize a Neural Network"""
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
-        if nx < 1:
+        elif nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(nodes) is not int:
+        elif type(nodes) is not int:
             raise TypeError("nodes must be an integer")
-        if nodes < 1:
+        elif nodes < 1:
             raise ValueError("nodes must be a positive integer")
 
         self.W1 = np.random.randn(nodes, nx)
-        self.b1 = 0
+        self.b1 = np.zeros(shape=(nodes, 1))
         self.A1 = 0
         self.W2 = np.random.randn(1, nodes)
         self.b2 = 0
