8edb9c589fe81a4f466a59851b5bd515c0657922
JamesTeeters <4925@holbertonstudents.com>
2023-02-08 19:05:48 -0600

    added forward propegation function

--- supervised_learning/0x01-classification/18-deep_neural_network.py
+++ supervised_learning/0x01-classification/18-deep_neural_network.py
@@ -1,1 +1,68 @@
 #!/usr/bin/env python3
+"""deep neural network"""
+import numpy as np
+
+
+def sig(x):
+    """sigmoid activation function"""
+    return (1 / (1 + np.exp(-x)))
+
+
+class DeepNeuralNetwork:
+    """class for DeepNeuralNetwork"""
+    def __init__(self, nx, layers):
+        """initialize DeepNeuralNetwork"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for i in range(self.__L):
+            if type(layers[i]) is not int or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
+            if i > 0:
+                self.__weights["W" + str(i + 1)] = np.random.randn(
+                    layers[i], layers[i-1])*np.sqrt(2/layers[i-1])
+                self.__weights["b" + str(i + 1)] =\
+                    np.zeros(shape=(layers[i], 1))
+            if i == 0:
+                self.__weights["W1"] = np.random.randn(
+                    layers[i], nx)*np.sqrt(2/nx)
+                self.__weights["b1"] = np.zeros(shape=(layers[i], 1))
+
+    @property
+    def weights(self):
+        """getter for the weights"""
+        return self.__weights
+
+    @property
+    def cache(self):
+        """getter for the cache"""
+        return self.__cache
+
+    @property
+    def L(self):
+        """getter for L"""
+        return self.__L
+
+    def forward_prop(self, X):
+        """forward propagate the deep neural network"""
+        for i in range(self.__L + 1):
+            if i == 0:
+                """store X in cache"""
+                self.__cache["A0"] = X
+            else:
+                """z is input modified with weights and bias"""
+                z = np.matmul(self.__weights["W" + str(i)],
+                              self.__cache["A" + str(i-1)])\
+                    + self.__weights["b" + str(i)]
+                """store activated output in the cache"""
+                self.__cache["A" + str(i)] = sig(z)
+
+        return self.__cache["A" + str(self.__L)], self.__cache
