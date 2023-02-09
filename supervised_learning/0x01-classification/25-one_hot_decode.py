435075576545c7e1bf5efdd8c78f2af2278b04f5
JamesTeeters <4925@holbertonstudents.com>
2023-02-08 19:58:19 -0600

    decode one_hot

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""converts a one-hot matrix into a vector of labels"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """decode a one-hot matrix into a vector of labels"""
+    try:
+        """classes = max number of classes"""
+        """m = number of examples"""
+        classes, m = one_hot.shape
+
+        vector = np.zeros(shape=(m,))
+        for i in range(m):
+            for j in range(classes):
+                if one_hot[j][i] == 1:
+                    vector[i] = j
+        return int(vector)
+    except Exception:
+        return None
